4e8196b105c355f84860f686b1bff80ef31fbcde
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-09-20 20:32:15 -0400

    added a menu functionality. Able to display departments, roles, and views as well as created prompts for other functions that do nothing at present

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,19 @@
-INSERT INTO department (name) VALUES
-  ('HR'),
-  ('Finance'),
-  ('Marketing'),
-  ('Engineering');
+-- Preset Department Data
+INSERT INTO department (name, id) VALUES
+  ('Engineering', 1),
+  ('Finance', 2),
+  ('Legal', 3),
+  ('Sales', 4);
 
--- Insert data into the role table
+-- Preset list of Roles, their salaries, and departments
 INSERT INTO role (title, salary, department) VALUES
-  ('HR Manager', 80000, 1),
+  ('Human Resource Manager', 80000, 3),
   ('Finance Analyst', 75000, 2),
-  ('Marketing Specialist', 70000, 3),
-  ('Software Engineer', 90000, 4),
-  ('Sales Manager', 85000, 3);
+  ('Marketing Specialist', 70000, 4),
+  ('Software Engineer', 90000, 1),
+  ('Sales Manager', 85000, 4);
 
--- Insert data into the employee table
+-- Preset employees, their first and last names and their roles as well as managers
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
   ('Mia', 'Thompson', 1, NULL),
   ('Oliver', 'Anderson', 2, 1),
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -13,11 +15,69 @@ app.use(express.json());
 const db = mysql.createConnection(
   {
     host: 'localhost',
-    // MySQL username,
     user: 'root',
-    // TODO: Add MySQL password here
     password: '46ToffeeKitty654!',
     database: 'employee_db'
   },
   console.log(`Connected to the employee_db database.`)
 );
+
+
+async function mainMenu() {
+    const { action } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: ['View all employees', 'Add employee', 'Update employee role', 'View All Roles', 'Add Role', 'View All Departments','Add Department', 'Exit'],
+        },
+    ]);
+
+    switch (action) {
+        case 'View all employees':
+            db.query('SELECT * FROM employee', (err, results) => {
+                if (err) {
+                  console.error('Error retrieving employees:', err);
+                } else {
+                  console.table(results);
+                }
+                mainMenu();
+              });
+            break;
+        case 'Add Employee':
+            break;
+        case 'Update Employee Role':
+            break;
+        case 'View All Roles':
+            db.query('SELECT * FROM role', (err, results) => {
+                if (err) {
+                  console.error('Error retrieving employees:', err);
+                } else {
+                  console.table(results);
+                }
+                mainMenu();
+              });
+            break;
+        case 'Add Role':
+            break;
+            case 'View All Departments':
+                db.query('SELECT * FROM department', (err, results) => {
+                    if (err) {
+                      console.error('Error retrieving employees:', err);
+                    } else {
+                      console.table(results);
+                    }
+                    mainMenu();
+                  });
+                break;
+        case 'Add Department':
+            break;
+        case 'Exit':
+            console.log('Goodbye!');
+            process.exit(0);
+            break;
+    }
+}
+  
+  // Call the main menu to start the application
+  mainMenu();
