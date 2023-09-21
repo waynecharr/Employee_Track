d7091468bb296e8ac775ae64cf7050003bedfc7c
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-09-21 16:13:31 -0400

    added query to the view employee case, view employees now displays properly

--- db/query.sql
+++ db/query.sql
@@ -1,21 +1,74 @@
-SELECT
-  role.id AS id, 
-  role.title AS title, 
-  role.salary AS salary, 
-  department.name AS department 
-FROM role
-JOIN department ON role.department = department.id;
+-- SELECT
+--   role.id AS id, 
+--   role.title AS title, 
+--   role.salary AS salary, 
+--   department.name AS department 
+-- FROM role
+-- JOIN department ON role.department = department.id;
+
+-- SELECT 
+--   employee.id AS id,
+--   employee.first_name AS first_name,
+--   employee.last_name AS last_name,
+--   role.title AS title,
+--   role.department AS department,
+--   role.salary AS salary,
+--   employee.manager AS manager
+-- FROM employee
+-- JOIN role ON employee.role_id = role.id;
+
+USE employee_db;
+
+-- SELECT 
+--   employee.id AS id,
+--   employee.first_name AS first_name,
+--   employee.last_name AS last_name,
+--   role.title AS title,
+--   department.name AS department,
+--   role.salary AS salary,
+--   m.first_name AS manager_first_name,
+--   m.last_name AS manager_last_name
+-- FROM employee
+-- JOIN role ON employee.role_id = role.id
+-- LEFT JOIN department ON role.department = department.id;
+-- LEFT JOIN employee m ON employee.manager_id = m.id;
+
+-- insert Into department(name)
+-- VALUES (response.department);
+
+USE employee_db;
 
 SELECT 
-  employee.id AS id,
-  employee.first_name AS first_name,
-  employee.last_name AS last_name,
-  role.title AS title,
-  role.department AS department,
-  role.salary AS salary,
-  employee.manager AS manager
-FROM employee
-JOIN role ON employee.role_id = role.id;
+  e.id AS id,
+  e.first_name AS first_name,
+  e.last_name AS last_name,
+  r.title AS title,
+  d.name AS department,
+  r.salary AS salary,
+  CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+FROM employee e
+JOIN role r ON e.role_id = r.id
+LEFT JOIN department d ON r.department = d.id
+LEFT JOIN employee m ON e.manager_id = m.id;
 
-insert Into department(name)
-VALUES (response.department);
+  const queryEmployees = `SELECT 
+  e.id AS id,
+  e.first_name AS first_name,
+  e.last_name AS last_name,
+  r.title AS title,
+  d.name AS department,
+  r.salary AS salary,
+  CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+FROM employee e
+JOIN role r ON e.role_id = r.id
+LEFT JOIN department d ON r.department = d.id
+LEFT JOIN employee m ON e.manager_id = m.id;`
+            db.query(queryEmployees, (err, results) => {
+                if (err) {
+                  console.error('Error retrieving employees:', err);
+                } else {
+                  console.table(results);
+                }
+                mainMenu();
+              });
+            break;
--- server.js
+++ server.js
@@ -24,84 +24,96 @@ const db = mysql.createConnection(
 
 
 async function mainMenu() {
-    const{ action } = await inquirer.prompt([
-        {
-            type: 'list',
-            name: 'action',
-            message: 'What would you like to do?',
-            choices: ['View all employees', 'Add employee', 'Update employee role', 'View All Roles', 'Add Role', 'View All Departments','Add Department', 'Exit'],
-        },
-    ]);
+  const { action } = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do?',
+      choices: ['View all employees', 'Add employee', 'Update employee role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Exit'],
+    },
+  ]);
 
-    switch (action) {
-        case 'View all employees':
-            db.query('SELECT * FROM employee', (err, results) => {
-                if (err) {
-                  console.error('Error retrieving employees:', err);
-                } else {
-                  console.table(results);
-                }
-                mainMenu();
-              });
-            break;
-        case 'Add Employee':
-            break;
-        case 'Update Employee Role':
-            break;
-        case 'View All Roles':
-          const queryRoles = `SELECT
+  switch (action) {
+    case 'View all employees':
+      const queryEmployees = `SELECT 
+          e.id AS id,
+          e.first_name AS first_name,
+          e.last_name AS last_name,
+          r.title AS title,
+          d.name AS department,
+          r.salary AS salary,
+          CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+        FROM employee e
+        JOIN role r ON e.role_id = r.id
+        LEFT JOIN department d ON r.department = d.id
+        LEFT JOIN employee m ON e.manager_id = m.id;`
+      db.query(queryEmployees, (err, results) => {
+        if (err) {
+          console.error('Error retrieving employees:', err);
+        } else {
+          console.table(results);
+        }
+        mainMenu();
+      });
+      break;
+    case 'Add Employee':
+      break;
+    case 'Update Employee Role':
+      break;
+    case 'View All Roles':
+      const queryRoles = `SELECT
           r.id AS id, 
           r.title AS title, 
           r.salary AS salary, 
           d.name AS department 
           FROM role r
           JOIN department d ON r.department = d.id;`
-            db.query(queryRoles, (err, results) => {
-                if (err) {
-                  console.error('Error retrieving employees:', err);
-                } else {
-                  console.table(results);
-                }
-                mainMenu();
-              });
-            break;
-        case 'Add Role':
-            break;
-            case 'View All Departments':
-                db.query('SELECT * FROM department', (err, results) => {
-                    if (err) {
-                      console.error('Error retrieving employees:', err);
-                    } else {
-                      console.table(results);
-                    }
-                    mainMenu();
-                  });
-                break;
-      case 'Add Department':
-        const response = await inquirer.prompt([
-          {
-            type: 'input',
-            name: 'department',
-            message: 'Add the name of your department:',
-          },
-        ]);
-        const addName = `insert Into department(name)
+      db.query(queryRoles, (err, results) => {
+        if (err) {
+          console.error('Error retrieving employees:', err);
+        } else {
+          console.table(results);
+        }
+        mainMenu();
+      });
+      break;
+    case 'Add Role':
+      break;
+    case 'View All Departments':
+      db.query('SELECT * FROM department', (err, results) => {
+        if (err) {
+          console.error('Error retrieving employees:', err);
+        } else {
+          console.table(results);
+        }
+        mainMenu();
+      });
+      break;
+    case 'Add Department':
+      const response = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'department',
+          message: 'Add the name of your department:',
+        },
+      ]);
+      const addName = `insert Into department(name)
           VALUES (?);`
-        db.query(addName, [response.department], (err, results) => {
-          if (err) {
-            console.error('Not a valid department name', err);
-          } else {
-            console.table(results);
-          }
-          mainMenu();
-        });
-        break;
-        case 'Exit':
-            console.log('Goodbye!');
-            process.exit(0);
-            break;
-    }
+      db.query(addName, [response.department], (err, results) => {
+        if (err) {
+          console.error('Not a valid department name', err);
+        } else {
+          console.table(results);
+        }
+        mainMenu();
+      });
+      break;
+    case 'Exit':
+      console.log('Goodbye!');
+      process.exit(0);
+      break;
+  }
 }
-  
-  // Call the main menu to start the application
-  mainMenu();
+
+// Call the main menu to start the application
+mainMenu();
